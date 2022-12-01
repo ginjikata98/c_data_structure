fb00d34cb2dc02b59b41299be9f909cd6248a2ac
Vũ Minh Dũng <dung.vu@finhay.com.vn>
2022-12-01 13:31:55 +0700

    update

--- libs/list.c
+++ libs/list.c
@@ -61,3 +61,35 @@ void List_addLast(List *list, void *item) {
   list->tail = newNode;
   list->length++;
 }
+
+void List_removeFirst(List *list) {
+  assert(list != null);
+  ListNode *nodeToFree = list->head;
+
+  if (list->head == list->tail) {
+    list->head = list->tail = null;
+  } else {
+    list->head = list->head->next;
+    list->head->prev = null;
+  }
+  list->length--;
+//  if (nodeToFree != null) {
+//    free(nodeToFree->item);
+//  }
+  free(nodeToFree);
+}
+
+void List_removeLast(List *list) {
+  assert(list != null);
+  ListNode *nodeToFree = list->tail;
+
+  if (list->head == list->tail) {
+    list->head = list->tail = null;
+  } else {
+    list->tail = list->tail->prev;
+    list->tail->next = null;
+  }
+  list->length--;
+//  free(nodeToFree->item);
+  free(nodeToFree);
+}
--- libs/main.c
+++ libs/main.c
@@ -9,11 +9,16 @@ void print(const void *e) {
 
 int main(void) {
 
-  u32 a = 10;
+  u32 a1 = 1;
+  u32 a2 = 2;
+  u32 a3 = 3;
 
   List *list = List_init();
-  List_addFirst(list, &a);
-  List_addLast(list, &a);
+  List_addFirst(list, &a1);
+  List_addLast(list, &a2);
+  List_addFirst(list, &a3);
+  List_removeFirst(list);
+  List_removeLast(list);
 
   List_print(list, print);
   return 0;
