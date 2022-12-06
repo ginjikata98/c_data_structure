65e5e3a65b5b7f5de364ad4a372893b7d97d5882
Vũ Minh Dũng <dung.vu@finhay.com.vn>
2022-12-06 09:18:56 +0700

    add tensorflow

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "libs.h"
+#include <tensorflow/c/c_api.h>
 
 f64 square(f64 x) {
   return x * x * x;
@@ -12,6 +13,7 @@ f64 grad(f64 (*f)(f64), f64 x) {
 
 int main(void) {
   printf("derivative of f(x) = x2 at 4 = %f", grad(square, 4));
+//  printf("Hello from TensorFlow C library version %s\n", TF_Version());
   return 0;
 
 }
