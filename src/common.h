4611b4bab6a3f929ca3e28f2d09f8ba7c27988e9
Vũ Minh Dũng <dung.vu@finhay.com.vn>
2022-12-06 15:33:02 +0700

    update

--- src/common.h
+++ src/common.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 #include <stddef.h>
-#include <assert.h>
+#include <ASSERT.h>
 #include <stdbool.h>
 
 #define null NULL
@@ -25,6 +25,60 @@ typedef uint8_t byte;
 
 typedef char *String;
 
+#define BIT(x)          (1<<(x))
+#define BIT_SET(x, p)     ((x)|(1<<(p)))
+#define BIT_CLEAR(x, p)   ((x)&(~(1<<(p))))
+#define BIT_AT(x, p)     (((x)>>(p))&1)
+#define BIT_TOGGLE(x, p)  ((x)^(1<<(p)))
+
+#define STR_(s)             #s
+#define STR(s)              STR_(s)
+#define CAT(str1, str2)      (str1 "" str2)
+#define PASTE_(a, b)         a##b
+#define PASTE(a, b)          PASTE_(a,b)
+#define PRINT_TOKEN(token)  printf(#token " is %d", token)
+
+#define PI                  3.141592654
+#define RAD2DEG(x)          ((x)/PI*180)
+#define DEG2RAD(x)          ((x)*PI/180)
+#define ALIGNB(x, align)    (((x) + ((align) - 1)) & ~((align) - 1))
+#define ALIGN(x, align)     ((((x) + ((align) - 1)) / (align)) * (align))
+#define FLOORB(x, align)    ((x) & ~((align) - 1))
+#define FLOOR(x, align)     (((x) / (align)) * (align))
+#define CEILB(x, align)     ALIGNB(x, align)
+#define CEIL(x, align)      ALIGN(x, align)
+#define CLIP(x, min, max)   (((x) < (min)) ? (min) : (((x) > (max)) ? (max) : (x)))
+#define UCLIP(x, max)       (((x) > (max)) ? (max) : (x))
+#define LCLIP(x, min)       (((x) < (min)) ? (min) : (x))
+#define MIN(x, y)           (((x) < (y)) ?  (x) : (y))
+#define MAX(x, y)           (((x) > (y)) ?  (x) : (y))
+#define ABS(x)              (((x) <  0) ? -(x) : (x))
+#define DIFF(a, b)           ABS((a)-(b))
+#define IS_NAN(x)            ((x) != (x))
+#define IMPLIES(x, y)       (!(x) || (y))
+#define SWAP(a, b)          do { a ^= b; b ^= a; a ^= b; } while ( 0 )
+#define SORT(a, b)          do { if ((a) > (b)) SWAP((a), (b)); } while (0)
+#define COMPARE(x, y)       (((x) > (y)) - ((x) < (y)))
+#define SIGN(x)             COMPARE(x, 0)
+#define IS_ODD(num)       ((num) & 1)
+#define IS_EVEN(num)      (!IS_ODD( (num) ))
+#define IS_BETWEEN(n, L, H)   ((unsigned char)((n) >= (L) && (n) <= (H)))
+
+#define LOG(x, fmt, ...)    if(x){printf("%s:%d: " fmt "\n",__FILE__, __LINE__,__VA_ARGS__);}
+#define TRY(x, s)            if(!(x)){printf("%s:%d:%s",__FILE__, __LINE__,s);}
+#ifndef DEBUG
+#define ASSERT(n)
+#else
+#define ASSERT(n)           if(!(n)) { \
+                            printf("%s - Failed ",#n); \
+                            printf("On %s ",__DATE__); \
+                            printf("At %s ",__TIME__); \
+                            printf("In File %s ",__FILE__); \
+                            printf("At Line %d\n",__LINE__); \
+                            exit(-1);}
+#endif
+
+
 typedef struct Node Node;
 typedef struct List List;
 
@@ -38,38 +92,38 @@ void List_addLast(List *list, void *item);
 void *List_removeFirst(List *list);
 void *List_removeLast(List *list);
 
-#define Vec(T) typedef struct { u32 size; u32 capacity; T *items;  } Vec_##T
+#define VEC(T) typedef struct { u32 size; u32 capacity; T *items;  } PASTE(Vec_, T)
 
-#define Vec_init(v, cap) \
+#define VEC_INIT(v, cap) \
 memset((v), 0, sizeof(*(v))); \
 (v)->capacity = (cap) == 0 ? 16 : (cap);                         \
 (v)->items = calloc((cap), sizeof((v)->items[0]));       \
-assert((v)->items != null)
+ASSERT((v)->items != null)
 
-#define Vec_destroy(v) free((v)->items); (v)->items=null
+#define VEC_DESTROY(v) free((v)->items); (v)->items=null
 
-#define Vec_add(v, e) \
+#define VEC_ADD(v, e) \
 if ((v)->size == (v)->capacity) { \
   (v)->items = realloc((v)->items, (v)->capacity * 2 * sizeof((v)->items[0])); \
-  assert((v)->items != null);\
+  ASSERT((v)->items != null);\
   (v)->capacity *= 2; \
 }                      \
 (v)->items[(v)->size++] = e
 
-#define Vec_at(v, i) (v)->items[(i)]
+#define VEC_AT(v, i) (v)->items[(i)]
 
-#define Vec_sort(v, cmp) (qsort((v)->items, (v)->size, sizeof(*(v)->items), cmp))
+#define VEC_SORT(v, cmp) (qsort((v)->items, (v)->size, sizeof(*(v)->items), cmp))
 
-#define Vec_pop(v) assert((v)->size > 0); (v)->size--
+#define VEC_POP(v) ASSERT((v)->size > 0); (v)->size--
 
-#define Vec_deleteAt(v, i) assert(i >= 0 && i < (v)->capacity); (v)->items[i] = (v)->items[--(v)->size]
+#define VEC_DELETE_AT(v, i) ASSERT(i >= 0 && i < (v)->capacity); (v)->items[i] = (v)->items[--(v)->size]
 
-Vec(u32);
-Vec(u64);
-Vec(i64);
-Vec(i32);
-Vec(f32);
-Vec(f64);
-Vec(String);
+VEC(u32);
+VEC(u64);
+VEC(i64);
+VEC(i32);
+VEC(f32);
+VEC(f64);
+VEC(String);
 
 #endif //C_DATA_STRUCTURE_LIBS_H
--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "common.h"
 #include <tensorflow/c/c_api.h>
+
+#define DEBUG
+#include "common.h"
 
 f64 square(f64 x) {
   return x * x * x;
@@ -11,29 +13,25 @@ f64 grad(f64 (*f)(f64), f64 x) {
   return (f(x + 1e-6) - f(x - 1e-6)) / (2e-6);
 }
 
-#define BIT(x)          (1<<(x))
-#define SETBIT(x, p)     ((x)|(1<<(p)))
-#define CLEARBIT(x, p)   ((x)&(~(1<<(p))))
-#define GETBIT(x, p)     (((x)>>(p))&1)
-#define TOGGLEBIT(x, p)  ((x)^(1<<(p)))
+
 
 int main(void) {
   Vec_u32 v;
-  Vec_init(&v, 16);
+  VEC_INIT(&v, 16);
   for (u32 i = 0; i < 10; ++i) {
-    Vec_add(&v, i);
+    VEC_ADD(&v, i);
   }
 
-  Vec_pop(&v);
-  Vec_pop(&v);
+  VEC_POP(&v);
+  VEC_POP(&v);
 
-  Vec_deleteAt(&v, 0);
-  Vec_deleteAt(&v, 0);
+  VEC_DELETE_AT(&v, 0);
+  VEC_DELETE_AT(&v, 0);
 
   for (u32 i = 0; i < v.size; ++i) {
-    printf("%d\n", Vec_at(&v, i));
+    printf("%d\n", VEC_AT(&v, i));
   }
 
-  Vec_destroy(&v);
+  VEC_DESTROY(&v);
   return 0;
 }
