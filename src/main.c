fbabaecbad6e67f768e32a0f466fef56df4b2e62
Tony <dung.vu@finhay.com.vn>
2022-12-06 22:14:59 +0700

    tf

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <tensorflow/c/c_api.h>
 
 #define DEBUG
+
 #include "macros.h"
 
 f64 square(f64 x) {
@@ -14,20 +15,39 @@ f64 grad(f64 (*f)(f64), f64 x) {
 }
 
 typedef struct NDArray {
-  byte *data;
-  u32 numOfDim;
-  u32 *dimensions;
-  u32 *strides;
+    byte *data;
+    u32 numOfDim;
+    u32 *dimensions;
+    u32 *strides;
 } NDArray;
 
 
+void NoOpDeallocator(void *data, size_t a, void *b) {}
+
 int main(void) {
-  List_f64 a;
-  List_init(&a);
+//  List_f64 a;
+//  List_init(&a);
+//
+//  Node_f64 n;
+//  Node_init(n, Node_f64);
 
-  Node_f64 n;
-  Node_init(n, Node_f64);
+  printf("Hello from TensorFlow C library version %s\n", TF_Version());
 
+  TF_Graph* Graph = TF_NewGraph();
+  TF_Status* Status = TF_NewStatus();
+  TF_SessionOptions* SessionOpts = TF_NewSessionOptions();
+  TF_Buffer* RunOpts = NULL;
+
+  i64 dims[] = {1, 1};
+  i64 data[] = {1, 2, 3, 4};
+  int ndata = sizeof(data);
+
+  TF_Tensor *t = TF_NewTensor(TF_INT64, dims, 2, data, ndata, &NoOpDeallocator, 0);
+
+  if (t != null) {
+    printf("TF_NewTensor is OK\n");
+  } else
+    printf("ERROR: Failed TF_NewTensor\n");
 
   return 0;
 }
