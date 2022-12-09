4ffda95269f1404845a8baf558adc3126ca6ad4b
Tony <dung.vu@finhay.com.vn>
2022-12-09 08:45:28 +0700

    update

--- src/main.c
+++ src/main.c
@@ -15,10 +15,10 @@ f64 grad(f64 (*f)(f64), f64 x) {
 }
 
 typedef struct NDArray {
-  byte *data;
-  u32 numOfDim;
-  u32 *dimensions;
-  u32 *strides;
+    byte *data;
+    u32 numOfDim;
+    u32 *dimensions;
+    u32 *strides;
 } NDArray;
 
 
@@ -26,8 +26,6 @@ void NoOpDeallocator(void *data, size_t a, void *b) {}
 
 
 void tf() {
-  printf("Hello from TensorFlow C library version %s\n", TF_Version());
-
   TF_Graph *graph = TF_NewGraph();
   TF_Status *status = TF_NewStatus();
   TF_SessionOptions *sessionOpts = TF_NewSessionOptions();
@@ -38,12 +36,29 @@ void tf() {
   f32 data[] = {1, 2, 3, 4};
   int dataSize = sizeof(data);
 
+  TF_Buffer* buf = TF_GetAllOpList();
+
+  TF_OperationDescription *desc = TF_NewOperation(graph, "Add", "a");
+
+  TF_Operation *ops = TF_FinishOperation(desc, status);
+
+  printf("%s", TF_Message(status));
+  assert(ops!=NULL && status == TF_OK);
+
+  TF_Output concat_dim_input = {ops, 0};
+  TF_AddInput(desc, concat_dim_input);
+  TF_Output values_inputs[1] = {ops, 0};
+  TF_AddInputList(desc, values_inputs, 1);
+
   TF_Tensor *t = TF_NewTensor(TF_FLOAT, dims, 2, data, dataSize, &NoOpDeallocator, 0);
   ASSERT(t != null);
 
+  printf("Hello from TensorFlow C library version %s\n", TF_Version());
 }
 
 int main(void) {
+
+  tf();
 //  List_f64 a;
 //  List_init(&a);
 //
