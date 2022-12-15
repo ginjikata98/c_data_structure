2a14464b4fd56fcc929bee34c5699da2732efc1f
Vũ Minh Dũng <dung.vu@finhay.com.vn>
2022-12-15 10:30:20 +0700

    update

--- src/executor.c
+++ src/executor.c
@@ -1,5 +1,18 @@
-//
-// Created by DUNG.VM on 15/12/2022.
-//
-
 #include "executor.h"
+
+struct VmExecutorWork {
+  VmRunnable runnable;
+  void *arg;
+  struct VmExecutorWork *next;
+};
+typedef struct VmExecutorWork VmExecutorWork;
+
+struct VmExecutor {
+  VmExecutorWork *workFirst;
+  VmExecutorWork *workLast;
+  VmMutex workMutex;
+  VmCondition workCondition;
+  VmU32 workingCount;
+  VmU32 threadCount;
+  VmBool running;
+};
--- src/executor.h
+++ src/executor.h
@@ -2,6 +2,12 @@
 #define MAIN_EXECUTOR_H
 
 #include "macro.h"
+#include "pthread.h"
+
+typedef pthread_t VmThread;
+typedef pthread_mutex_t VmMutex;
+typedef pthread_cond_t VmCondition;
+
 
 typedef struct VmExecutor VmExecutor;
 typedef void (*VmRunnable)(void *arg);
